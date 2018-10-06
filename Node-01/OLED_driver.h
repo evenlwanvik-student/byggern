c147eb21c223d8ad5ebf3fbcfa207c3e26870c77
evenlwanvik-student <42798352+evenlwanvik-student@users.noreply.github.com>
2018-10-06 17:10:04 +0200

    Update OLED_driver.h

--- Node-01/OLED_driver.h
+++ Node-01/OLED_driver.h
@@ -1,21 +1,30 @@
+#ifndef OLED_DRIVER_H_
+#define OLED_DRIVER_H_
 
-
-void init_OLED();
-
+void OLED_init();
 void OLED_print_char(char print_c);
+void OLED_printf(char *data, uint8_t line, uint8_t col);
+void OLED_align_centre(char* input_str);
+void OLED_goto_pos(uint8_t line, uint8_t column);
+void OLED_print_welcome_P(const char* data);
+uint8_t OLED_fontsize_get();
 
 typedef enum {
     FONT8,
     FONT5,
     FONT4
-} font_enum;
+} font_enum_t;
+
+void OLED_alt_font_size(font_enum_t size);
 
 typedef enum{
     HORIZONTAL_MODE,
     VERTICAL_MODE,
     PAGE_MODE
-} adressing_mode;
+} adressing_mode_t;
 
 typedef struct {
-	int row, col;
+	int line, col;
 } oled_position_t;
+
+#endif /* OLED_DRIVER_H_ */
